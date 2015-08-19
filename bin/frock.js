a9f9e423f3f592ca9c90293c551096e31ffc7514
fardog <nate@milkandtang.com>
2015-08-19 16:58:43 -0700

    support daemonizing, stub commands

--- bin/frock.js
+++ bin/frock.js
@@ -4,11 +4,20 @@ var path = require('path')
 
 var cli = require('../lib/cli')
 
-cli(process.argv.slice(2), function (err) {
+cli(process.argv.slice(2), function (err, options) {
   if (err) {
     throw err
   }
 
-  var logo = fs.createReadStream(path.join(__dirname, 'logo.txt'))
-  logo.pipe(process.stdout)
+  if (options.launched) {
+    var logo = fs.createReadStream(path.join(__dirname, 'logo.txt'))
+    logo.pipe(process.stdout)
+  } else if (options.help) {
+    var help = fs.createReadStream(path.join(__dirname, 'help.txt'))
+    help.pipe(process.stdout)
+    help.on('end', process.exit.bind(process, 1))
+  } else if (options.version) {
+    console.log(require('../package.json').version)
+    process.exit(1)
+  }
 })
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
+import net from 'net'
 
+import tmpDir from 'os-tmpdir'
 import minimist from 'minimist'
 import find from 'fs-find-root'
 
@@ -8,13 +10,31 @@ import createFrockInstance from './'
 
 export default processCli
 
+const DEFAULT_SOCKET_NAME = 'frock.sock'
+
 function processCli (args, ready) {
-  const argv = minimist(args)
+  const options = {
+    alias: {
+      command: 'c',
+      patch: 'p',
+      socket: 's'
+    }
+  }
+  const argv = minimist(args, options)
+
+  if (argv.version || argv.help) {
+    return ready(null, {version: argv.version, help: argv.help})
+  }
 
   let file = argv._[0]
+  let socket = argv.socket ? path.resolve(socket) : null
+
+  if (argv.command && socket) {
+    connectServer(socket, argv, ready)
+  }
 
   // if we weren't given a frockfile path, find one
-  if (!argv._[0]) {
+  if (!file) {
     find.file('frockfile.json', process.cwd(), (err, foundFile) => {
       if (err) {
         return ready(err)
@@ -31,6 +51,10 @@ function processCli (args, ready) {
   fs.readFile(file, onFrockfile)
 
   function onFrockfile (err, _frockfile) {
+    if (err) {
+      return ready(err)
+    }
+
     argv.pwd = path.dirname(file)
 
     let frockfile
@@ -41,10 +65,79 @@ function processCli (args, ready) {
       throw new Error(`Error parsing frockfile: ${e}`)
     }
 
-    const frock = createFrockInstance(frockfile, argv)
+    if (!socket) {
+      socket = path.join(tmpDir(), frockfile.socketName || DEFAULT_SOCKET_NAME)
+    }
 
-    frock.run(() => {
-      ready(err, {argv, frock, frockfile})
+    if (argv.command) {
+      connectServer(socket, argv, ready)
+
+      return
+    }
+
+    startServer()
+
+    function startServer () {
+      const frock = createFrockInstance(frockfile, argv)
+      const server = net.createServer(client => {
+        client.on('data', data => {
+          const command = data.toString()
+
+          if (command === 'restart') {
+            console.log('would restart')
+          } else if (command === 'stop') {
+            console.log('would stop')
+          } else {
+            console.log('unrecognized command', command)
+          }
+
+          client.write('success')
+        })
+      })
+
+      server.listen(socket, () => {
+        // on shutdown we need to murder the socket
+        process.on('SIGINT', () => {
+          console.log('cleaning up...')
+          server.close(() => {
+            try {
+              fs.unlinkSync(socket)
+            } finally {
+              process.exit(1)
+            }
+          })
+
+          setTimeout(() => {
+            console.log('failed to clean up in a reasonable time, forcing...')
+            try {
+              fs.unlinkSync(socket)
+            } finally {
+              process.exit(1)
+            }
+          }, 2000)
+        })
+
+        frock.run(() => {
+          ready(null, {argv, frock, frockfile, launched: true})
+        })
+      })
+    }
+  }
+
+  function connectServer (path, argv, ready) {
+    const client = net.connect({path}, () => {
+      console.log('connected')
+      client.write(argv.command)
+    })
+
+    client.on('data', data => {
+      console.log(data.toString())
+      client.end()
+    })
+
+    client.on('end', () => {
+      console.log('done')
+      ready(null, {success: true})
     })
   }
 }
